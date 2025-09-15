87bcbaa54ad39accd20d1f84c449678ba4470f2e
Felix Nordén <felixnorden@gmail.com>
2025-09-15 10:47:20 +0100

    fix: move dashboard to landing page

--- src/routes/testnet.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import TestnetPage from "../components/pages/TestnetPage";
-
-export const Route = createFileRoute("/testnet")({
-  component: TestnetPage,
-});
